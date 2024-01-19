4ed8a4d9b93454e49d46006733ec5b0b3a8f0a51
Meet Makwana <meetmakwana203@gmail.com>
2024-01-19 11:32:46 +0530

    Added new sample object route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 // empty parameter to @Controller is triggered at no router /
@@ -10,6 +10,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Header("Content-Type", "application/json")
   getObject(): object {
     return this.appService.getObject();
   }
